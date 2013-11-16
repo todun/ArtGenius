729e7f0c4989627881417baf55179fe346e48324
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-11-16 17:30:14 -0500

    we got users dawg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if !user_signed_in?
+      redirect_to :new_user_session
+    end
+    @users = User.all
+  end
+
+  def new
+    respond_to do |format|
+      format.html { render :layout => "sessions" }
+    end
   end
 end
