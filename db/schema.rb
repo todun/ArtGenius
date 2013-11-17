0261862b8d640282b04b432cd90665186d32dca0
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-11-17 05:42:00 -0500

    added artist and year

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131117002052) do
+ActiveRecord::Schema.define(:version => 20131117103430) do
 
   create_table "comments", :force => true do |t|
     t.integer  "x1"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20131117002052) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "artist"
+    t.integer  "year"
   end
 
   create_table "users", :force => true do |t|
